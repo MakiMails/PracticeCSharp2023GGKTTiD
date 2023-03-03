5ed4d376f40841e5209fe146303313e60f2d29f2
MakiMails <salivonhik1990@gmail.com>
2023-03-03 14:22:54 +0300

    _

---
+++ Prac9/MyLibrary/Новая папка/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Новая_папка
+{
+    internal class Class1
+    {
+    }
+}
