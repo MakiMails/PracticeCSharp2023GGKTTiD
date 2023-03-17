a360793c825abdd5e1fdd76485b625c89381bdfa
MakiMails <salivonhik1990@gmail.com>
2023-03-17 19:13:13 +0300

    _

---
+++ Prac21/z1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Task.Run(() => Console.WriteLine(g(num)));
+
+async Task<int> g(int num)=>num % 10 * 100 + num / 10;
