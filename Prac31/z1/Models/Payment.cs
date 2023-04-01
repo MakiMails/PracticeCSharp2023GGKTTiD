800f89ceef071cb41fb6f8765a9994ec8c4c8c62
MakiMails <salivonhik1990@gmail.com>
2023-04-01 09:07:26 +0300

    _

---
+++ Prac31/z1/Models/Payment.cs
@@ -0,0 +1,35 @@
+﻿namespace z1.Models
+{
+    internal class Payment
+    {
+        private int _codeTravelPackage;
+        private DateOnly _datePayments;
+        private decimal _sum;
+
+        public int CodeTrravelPackage
+        {
+            get => _codeTravelPackage;
+            set => _codeTravelPackage = value;
+        }
+
+        public DateOnly DatePayments
+        {
+            get => _datePayments;
+            set => _datePayments = value;
+        }
+
+        public decimal Sum
+        {
+            get => _sum;
+            set => _sum = value;
+        }
+
+        public Payment(int codeTravelPackage, DateOnly datePayments,
+            decimal sum)
+        {
+            _codeTravelPackage = codeTravelPackage;
+            _datePayments = datePayments;
+            _sum = sum;
+        }
+    }
+}
